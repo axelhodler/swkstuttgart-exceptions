44dd0d76c666142d63f1d70d1c99e60ad5de1802
Axel Hodler <derxor@gmail.com>
2017-04-17 10:50:56 +0200

    extensions

--- person.hs
+++ person.hs
@@ -1,17 +1,18 @@
+-- Taken from Haskellbook 0.12.0 p. 460 and slightly changed
+
 module Person where
 
 type Name = String
 type Age = Integer
-type ValidatePerson a = Either PersonInvalid a
+type ValidatePerson a = Either [PersonInvalid] a
 
 data Person = Person Name Age deriving Show
 
-data PersonInvalid = AgeTooLow deriving (Eq, Show)
+data PersonInvalid = NameEmpty
+                   | AgeTooLow deriving (Eq, Show)
 
-ageOkay :: Age -> Either PersonInvalid Age
-ageOkay age = if age >= 0 then Right age else Left AgeTooLow
+ageOkay :: Age -> Either [PersonInvalid] Age
+ageOkay age = if age >= 0 then Right age else Left [AgeTooLow]
 
-isAgeOkay :: Age -> Bool
-isAgeOkay x = case ageOkay x of
-  (Left _) -> False
-  (Right _) -> True
+nameOkay :: Name -> Either [PersonInvalid] Name
+nameOkay name = if name /= "" then Right name else Left [NameEmpty]
